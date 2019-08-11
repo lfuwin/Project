a7097cf9125bd099d487663605a7858a0a681706
lfuwin <54007931+lfuwin@users.noreply.github.com>
2019-08-11 18:27:56 -0400

    Create items.php

---
+++ html/items.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<style>
+ul.products li {
+    width: 200px;
+    display: inline-block;
+    vertical-align: top;
+    *display: inline;
+    *zoom: 1;
+}
+</style>
+</head>
+<body>
+
+
+<?php
+if (!$link = mysql_connect('localhost', 'fecetaul_Project', 'mypassword')) {
+    echo 'Could not connect to mysql';
+    exit;
+}
+if (!mysql_select_db('fecteaul_Project', $link)) {
+    echo 'Could not select database';
+    exit;
+}
+$sql    = 'SELECT * FROM items';
+$result = mysql_query($sql, $link);
+if (!$result) {
+    echo "DB Error, could not query the database\n";
+    echo 'MySQL Error: ' . mfnameysql_error();
+    exit;
+}
+echo '<ul class="products">';
+while ($rs = mysql_fetch_assoc($result)) {
+?>   <a href="#<?php echo $rs["item_id"]?>">
+           <img src="data:image/jpeg;base64,<?php echo base64_encode( $rs["item_pic"]); ?> ">
+            <h4> <?php echo $rs["item_desc"] ?></h4>
+            
+        </a>
+<?php
+}
+echo '</ul>';
+?>
+</body>
